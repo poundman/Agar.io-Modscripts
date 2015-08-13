51c2b0848c6ff17c2d86b88a5ac51ad624deb9e3
OmniPhaser <OmniPhaser@users.noreply.github.com>
2015-08-13 10:28:13 -0700

    Update ph4nt0m_pwn-agariomods-loader.js

--- latest/ph4nt0m_pwn-agariomods-loader.js
+++ latest/ph4nt0m_pwn-agariomods-loader.js
@@ -1,13 +1,11 @@
 // ==UserScript==
-// @name        ph4nt0m_pwn_agariomods_script
-// @namespace	  ph4nt0m_pwn_agariomods_script
-// @version      1.9.9
-// @description  ph4nt0m_pwn_agariomods_script
-// @author       electronoob
-// @match        http://agar.io/
-// @match        http://agar.io/#*
-// @match        http://agar.io/?*
-// @grant        none
+// @name Open CodeProject Links
+// @namespace http://hibbard.eu/
+// @version 0.1
+// @description Opens all links in the CodeProject newsletter 
+// @match http://www.codeproject.com/script/Mailouts/*
+// @copyright 2012+, hibbard.eu
+// @require http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
 
 var script = document.createElement('script');
