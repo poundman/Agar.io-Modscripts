6029b51a86e660948021ad62bc7ac5a0b0b2d987
OmniPhaser <OmniPhaser@users.noreply.github.com>
2015-08-13 10:25:27 -0700

    Update ph4nt0m_pwn-agariomods-loader.js

--- latest/ph4nt0m_pwn-agariomods-loader.js
+++ latest/ph4nt0m_pwn-agariomods-loader.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         ph4nt0m_pwn Agario modscript
-// @namespace	 ph4nt0m_pwn Agario modscript
+// @name        ph4nt0m_pwn_agariomods_script
+// @namespace	  ph4nt0m_pwn_agariomods_script
 // @version      1.9.9
-// @description  ph4nt0m_pwn Agario modscript
-// @author       ph4nt0m_pwn
+// @description  ph4nt0m_pwn_agariomods_script
+// @author       electronoob
 // @match        http://agar.io/
 // @match        http://agar.io/#*
 // @match        http://agar.io/?*
@@ -13,3 +13,11 @@
 var script = document.createElement('script');
 script.src = document.location.protocol+"//agariomods.com/mods.js";
 (document.body || document.head || document.documentElement).appendChild(script);
+
+/*
+repo:
+https://github.com/electronoob/agarmods
+
+http://www.agariomods.com
+
+*/
