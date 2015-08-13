405ba1790fec011e27b86daaf006ec8a92980007
OmniPhaser <pwnetwork@outlook.com>
2015-08-13 09:53:39 -0700

    Add ph4nt0m_pwn agariomods.com pwn

---
+++ v0.1/ph4nt0m_pwn-agariomods-loader.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         ph4nt0m_pwn Agario modscript
+// @namespace	 ph4nt0m_pwn Agario modscript
+// @version      1.9.9
+// @description  ph4nt0m_pwn Agario modscript
+// @author       ph4nt0m_pwn
+// @match        http://agar.io/
+// @match        http://agar.io/#*
+// @match        http://agar.io/?*
+// @grant        none
+// ==/UserScript==
+
+var script = document.createElement('script');
+script.src = document.location.protocol+"//agariomods.com/mods.js";
+(document.body || document.head || document.documentElement).appendChild(script);
